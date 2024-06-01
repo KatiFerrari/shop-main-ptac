2d96572bb846ac3602fbff9e3f42628ad70706e3
KatiFerrari <katielidasilvaferrari26@gmail.com>
2024-06-01 16:31:13 -0400

    dia-01-09-adicionei a frase de descrição da empresa, as logos da empresa, e também utilizei o css para deixar a imagem na direita e mudei a fonte da letra para a mesma do header.

--- src/app/componentes/Footer.js
+++ src/app/componentes/Footer.js
@@ -1,8 +1,20 @@
 import styles from "./footer.module.css";
+import Image from "next/image"
 export default function Footer(){
     return(
           <footer className={styles.footer}>
-            <h1>Fim</h1>
+            <div>
+            <p className={styles.frase}>A Shop Main, inaugurada em 25 de fevereiro de 2007, oferece produtos de qualidade 
+             com excelente atendimento e preços competitivos.</p>
+      
+            <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"/>
+            <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/4494/4494497.png"/>
+            <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/174/174848.png"/>
+            <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/4494/4494495.png"/>
+            </div>
+            <div>
+            <Image className={styles.imagem_direita} width={50} height={50} src="https://cdn-icons-png.flaticon.com/128/4273/4273228.png"/>
+            </div>
           </footer>
     );
 };
