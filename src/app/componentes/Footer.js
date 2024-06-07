4f6e02ad6425f9c4b3b7e19c11d4e4cba8005448
KatiFerrari <katielidasilvaferrari26@gmail.com>
2024-06-07 14:55:49 -0400

    aula-dia-07-06-atualizado

--- src/app/componentes/Footer.js
+++ src/app/componentes/Footer.js
@@ -2,19 +2,19 @@ import styles from "./footer.module.css";
 import Image from "next/image"
 export default function Footer(){
     return(
-          <footer className={styles.footer}>
-            <div>
-            <p className={styles.frase}>A Shop Main, inaugurada em 25 de fevereiro de 2007, oferece produtos de qualidade 
-             com excelente atendimento e preços competitivos.</p>
-      
+     <footer className={styles.footer}>
+          <div className={styles.cabecalho2}>
+              <p  className={styles.frase}><strong>A Shop Main, inaugurada em 25 de fevereiro de 2007, oferece produtos de qualidade 
+              com excelente atendimento e preços competitivos.</strong></p>
+           </div> 
+
+          <div className={styles.cabecalho3}>
             <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png"/>
             <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/4494/4494497.png"/>
             <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/174/174848.png"/>
             <Image width={30} height={30} src="https://cdn-icons-png.flaticon.com/128/4494/4494495.png"/>
-            </div>
-            <div>
             <Image className={styles.imagem_direita} width={50} height={50} src="https://cdn-icons-png.flaticon.com/128/4273/4273228.png"/>
-            </div>
-          </footer>
+          </div>
+      </footer>
     );
 };
--- src/app/componentes/Main.js
+++ src/app/componentes/Main.js
@@ -1,12 +1,49 @@
+'use client'
 import Image from "next/image"
 import styles from "./main.module.css"
-export default async function Main(){
-    const response = await fetch("https://fakestoreapi.com/products");
-    const data = await response.json();
+import { useEffect, useState } from "react";
+export default function Main(){
+
+  const [listProduct, setListProduct] = useState([]);
+  useEffect(()=> {
+    const getProduct = async()=>{
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+
+     setListProduct(data);
+    }
+    getProduct();
+  }, []);
+
+   const orderAz = () =>{
+    const newList = [...listProduct].sort( (a,b)=>
+          a.title.localeCompare(b.title)
+   );
+    setListProduct(newList);
+   }
+
+   const orderZa = () =>{
+    let newList = [...listProduct].sort( (a,b)=>
+          a.title.localeCompare(b.title)
+   );
+    newList = newList.reverse();
+    setListProduct(newList);
+   }
+
+   const orderPreco = () => {
+    const newList = [...listProduct].sort((a, b) => b.price - a.price);
+    setListProduct(newList);
+  }
 
       return (
+        <>
+        <div>
+          <button onClick={orderAz}>Az</button>
+          <button onClick={orderZa}>Za</button>
+          <button onClick={orderPreco}>Preçp maior</button>
+        </div>
         <main className={styles.main}>
-          {data.map((products)=> 
+          {listProduct.map((products)=> 
                 <div className={styles.card} key={products.id}>
                   <h2>{products.title}</h2>
                   <p>Preço: R${products.price}</p>
@@ -18,5 +55,6 @@ export default async function Main(){
 
           )}
         </main>
+        </>
       );
 }
